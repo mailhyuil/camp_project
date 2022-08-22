4468cc321f4aa58e4e1107aa644ec92dc7b983ab
mailhyuil <mailhyuil@gmail.com>
2022-08-22 20:58:37 +0900

    map added

--- spring-legacy-project-jpa-mybatis/src/main/java/com/sb/camp/controller/CampController.java
+++ spring-legacy-project-jpa-mybatis/src/main/java/com/sb/camp/controller/CampController.java
@@ -1,11 +1,16 @@
 package com.sb.camp.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sb.camp.domain.Camp;
 import com.sb.camp.service.CampService;
@@ -28,11 +33,22 @@ public class CampController {
 	}
 	
 	@GetMapping("/detail")
-	public String home(Model model, String id) {
+	public String detail(Model model, String id) {
 		Camp camp = campService.findCampById(id);
 		weatherService.getWeatherByLatAndLon(model, camp.getMapY(), camp.getMapX());
 		model.addAttribute("CAMP", campService.findCampById(id));
 		return "/camp/detail";
+	}
+	
+	@ResponseBody
+	@GetMapping("/getLatAndLon")
+	public ResponseEntity<Map<String,String>> getLatAndLon(String id) {
+		Camp camp = campService.findCampById(id);
+		Map<String, String> latlon = new HashMap<>();
+		latlon.put("lat", camp.getMapY());
+		latlon.put("lon", camp.getMapX());
+		return ResponseEntity
+                .ok(latlon);
 	}
 	
 }
